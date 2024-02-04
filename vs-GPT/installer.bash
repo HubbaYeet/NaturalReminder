c1b8e8825d2bc3d42738f4870eb7a72c98c6e456
Vaughn <79726680+HubbaYeet@users.noreply.github.com>
2024-02-03 20:35:00 -0800

    Delete vs-GPT directory

--- vs-GPT/installer.bash
+++
@@ -1,9 +0,0 @@
-# clone git repo
-git clone https://github.com/AlexSenties/vs-GPT.git
-cd vs-GPT
-
-# run program
-python3 main.py
-
-#program finished
-echo "Process completed! Call 'python3 main.py' to run the program again."
